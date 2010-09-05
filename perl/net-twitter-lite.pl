a94865e985bdff6be0690043188621afe1f33957
koba04 <koba0004@gmail.com>
2010-09-06 00:29:42 +0900

    decode query

--- perl/net-twitter-lite.pl
+++ perl/net-twitter-lite.pl
@@ -5,17 +5,16 @@ use utf8;
 use strict;
 use warnings;
 
+use Encode;
 use Net::Twitter::Lite;
 
 binmode STDOUT, ':utf8';
 
-my $query = $ARGV[0];
-
+my $query = decode_utf8($ARGV[0]);
 say "search query => $query";
 
 my $nt = Net::Twitter::Lite->new;
-
-my $result = $nt->search($query);
+my $result = $nt->search(decode_utf8($query));
 for my $tweet (@{$result->{results}}) {
     say '【' . $tweet->{from_user} . ' 】' . $tweet->{text};
 }
