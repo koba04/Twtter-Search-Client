7d643066f4fe763d51edde7b20dc14e6bf4429f6
koba04 <koba0004@gmail.com>
2010-09-05 17:36:34 +0900

    add java program

---
+++ java/Search.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+import org.json.simple.*;
+
+public class Search {
+
+	/**
+	 * @param args	search query
+	 */
+	public static void main(String[] args) {
+
+		try {
+			// URIの組立て
+			String query = URLEncoder.encode(args[0], "UTF-8");
+			String api = "http://search.twitter.com/search.json?q=" + query;
+			URL uri = new URL(api);
+
+			// HTTP Request
+			HttpURLConnection request = (HttpURLConnection) uri.openConnection();
+			request.setRequestMethod("GET");
+			BufferedReader response = new BufferedReader(new InputStreamReader(
+					request.getInputStream()));
+
+			// Response Parse
+			StringBuilder responseBody = new StringBuilder();
+			String str;
+			while ((str = response.readLine()) != null) {
+				responseBody.append(str);
+			}
+			response.close();
+			request.disconnect();
+			
+			JSONObject json = (JSONObject) JSONValue.parse(responseBody.toString());
+			List<JSONObject> tweets = (List<JSONObject>) json.get("results");
+			
+			System.out.println("Parse is =>" + tweets.toString());
+			
+			for (JSONObject twets : tweets) {
+				System.out.println("【" + twets.get("from_user") + "】"
+						+ twets.get("text"));
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
